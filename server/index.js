459862eeaecea515c07afb756c343bb6b5afba20
nithinK-142 <nithin.sagar.359@gmail.com>
2024-05-04 11:55:10 +0530

    feat: ffmpeg.

--- server/index.js
+++ server/index.js
@@ -1,15 +1,14 @@
 import express from "express";
 import cors from "cors";
 import { upload } from "./multer.js";
+import { v4 as uuidv4 } from "uuid";
+import fs from "fs";
+import { exec } from "child_process"; // illegal usage
 
 const app = express();
 const PORT = 8080;
 
-app.use(
-  cors({
-    origin: ["http://localhost:5173"],
-  })
-);
+app.use(cors({ origin: ["http://localhost:5173"] }));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -19,7 +18,36 @@ app.get("/", function (req, res) {
   res.json({ message: "Video Streaming..." });
 });
 app.post("/upload", upload.single("file"), function (req, res) {
-  res.json({ message: "file uploaded." });
+  const lessonId = uuidv4();
+  const videoPath = req.file.path;
+  const outputPath = `./uploads/chunks/${lessonId}`;
+  const hlsPath = `${outputPath}/index.m3u8`;
+  console.log("hlsPath", hlsPath);
+
+  if (!fs.existsSync(outputPath)) {
+    fs.mkdirSync(outputPath, { recursive: true });
+  }
+
+  // ffmpeg
+  const ffmpegCommand = `ffmpeg -i ${videoPath} -codec:v libx264 -codec:a aac -hls_time 10 -hls_playlist_type vod -hls_segment_filename "${outputPath}/segment%03d.ts" -start_number 0 ${hlsPath}
+
+`;
+
+  // no queue usage, not to be used in production
+  exec(ffmpegCommand, (error, stdout, stderr) => {
+    if (error) {
+      console.log(`exec error: ${error}`);
+    }
+    console.log(`stdout: ${stdout}`);
+    console.log(`stderr: ${stderr}`);
+    const videoUrl = `http://localhost:8080/uploads/courses/${lessonId}/index.m3u8`;
+
+    res.json({
+      message: "Video converted to HLS format",
+      videoUrl: videoUrl,
+      lessonId: lessonId,
+    });
+  });
 });
 
 app.listen(PORT, function () {
